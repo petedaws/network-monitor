c192a0791b5797e3ede7969f9c4b63fb0ee11b00
Peter <petedaws@gmail.com>
2013-08-09 16:09:02 +1000

    more debug code

--- network-monitor.py
+++ network-monitor.py
@@ -6,10 +6,13 @@ nm = nmap.PortScanner()
 mac_list = []
 
 while 1:
+  print 'running scan..',
   r = nm.scan('192.168.1.1/24 -sn')
+  print 'complete'
   current_mac_list = [i['addresses'].get('mac') for i in r['scan'].values()]
   new_macs = set(current_mac_list) - set(mac_list)
   lost_macs = set(mac_list) - set(current_mac_list)
+  mac_list = current_mac_list
   if len(new_macs):
       print 'New hosts found %s' % new_macs
   if len(lost_macs):
