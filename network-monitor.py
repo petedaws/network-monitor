2a3b37fcab9ac42e8a4e07478daa3cea227e5c2c
Peter <petedaws@gmail.com>
2013-08-09 17:30:05 +1000

    support for new host detection

--- network-monitor.py
+++ network-monitor.py
@@ -18,13 +18,18 @@ while 1:
   print 'running scan..',
   current_scan = nm.scan('192.168.1.1/24 -sn')
   print 'complete'
-  new_macs = set(get_mac_list(current_scan)) - set(get_mac_list(last_scan))
-  lost_macs = set(get_mac_list(last_scan)) - set(get_mac_list(current_scan))
+  host_join = set(get_mac_list(current_scan)) - set(get_mac_list(last_scan))
+  host_left = set(get_mac_list(last_scan)) - set(get_mac_list(current_scan))
+  host_new = set(get_mac_list(cumulative_scan)) - set(get_mac_list(current_scan))
   last_scan = current_scan
   cumulative_scan.update(current_scan)
   open('cumulative_scan.txt','wb').write(pprint.pformat(cumulative_scan))
-  if len(new_macs):
-      print 'New hosts found %s' % new_macs
-  if len(lost_macs):
-      print 'Hosts lost %s' % lost_macs
+  if len(host_join):
+      #print 'Host Joined %s' % host_join
+      pass
+  if len(host_left):
+      #print 'Host Left %s' % host_left
+      pass    
+  if len(host_new):
+      print 'New Host detected %s' % host_new
   time.sleep(10)
