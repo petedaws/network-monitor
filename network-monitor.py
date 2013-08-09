86c9adfce0f13ca6ab9182030f032ae1b9387dfe
Peter <petedaws@gmail.com>
2013-08-09 18:04:56 +1000

    reformated the scan dict

--- network-monitor.py
+++ network-monitor.py
@@ -8,19 +8,23 @@ nm = nmap.PortScanner()
 last_scan = {}
 cumulative_scan = {}
 
-def get_mac_list(scan):
+def scan_reformat(scan):
+  sr = {}
   if 'scan' in scan:
-    return [i['addresses'].get('mac') for i in scan['scan'].values()]
+    for host in scan['scan']:
+      sr[host['addresses']['mac']] = host
+      return sr
   else:
-    return []
+    return {}
 
 while 1:
   print 'running scan..',
-  current_scan = nm.scan('192.168.1.1/24 -sn')
+  s = nm.scan('192.168.1.1/24 -sn')
   print 'complete'
-  host_join = set(get_mac_list(current_scan)) - set(get_mac_list(last_scan))
-  host_left = set(get_mac_list(last_scan)) - set(get_mac_list(current_scan))
-  host_new = set(get_mac_list(cumulative_scan)) - set(get_mac_list(current_scan))
+  current_scan = scan_reformat(s)
+  host_join = set(current_scan.keys()) - set(last_scan.keys())
+  host_left = set(last_scan.keys()) - set(current_scan.keys())
+  host_new = set(cumulative_scan.keys()) - set(current_scan.keys())
   last_scan = current_scan
   cumulative_scan.update(current_scan)
   open('cumulative_scan.txt','wb').write(pprint.pformat(cumulative_scan))
