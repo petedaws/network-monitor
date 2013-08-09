9ef8c92bb24ee310662155fd22383f9fff1b4985
Peter <petedaws@gmail.com>
2013-08-09 17:17:41 +1000

    keep all scan details and output the cumulative scan

--- network-monitor.py
+++ network-monitor.py
@@ -2,18 +2,24 @@ import nmap
 import pprint
 import time
 import sys
+import pprint
 nm = nmap.PortScanner()
 
-mac_list = []
+last_scan = []
+cumulative_scan = {}
+
+def get_mac_list(scan):
+  return [i['addresses'].get('mac') for i in scan['scan'].values()]
 
 while 1:
-  sys.stdout.write('running scan..')
-  r = nm.scan('192.168.1.1/24 -sn')
+  print 'running scan..',
+  current_scan = nm.scan('192.168.1.1/24 -sn')
   print 'complete'
-  current_mac_list = [i['addresses'].get('mac') for i in r['scan'].values()]
-  new_macs = set(current_mac_list) - set(mac_list)
-  lost_macs = set(mac_list) - set(current_mac_list)
-  mac_list = current_mac_list
+  new_macs = set(get_mac_list()) - set(get_mac_list(last_scan))
+  lost_macs = set(get_mac_list(last_scan)) - set(get_mac_list(current_scan))
+  last_scan = current_scan
+  cumulative_scan.update(current_scan)
+  open('cumulative_scan.txt','wb').write(pprint.pprint(cumulative_scan)
   if len(new_macs):
       print 'New hosts found %s' % new_macs
   if len(lost_macs):
