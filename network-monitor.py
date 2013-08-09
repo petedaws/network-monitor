b303e67b401e5dcbe32cc39f4d3cfc409e0bc05e
Peter <petedaws@gmail.com>
2013-08-09 14:27:52 +1000

    continuous scanning

--- network-monitor.py
+++ network-monitor.py
@@ -1,6 +1,10 @@
 import nmap
 import pprint
+import time
 nm = nmap.PortScanner()
-nm.scan('192.168.1.1/24 -sn')
-hosts = nm.all_hosts()
-pprint.pprint(hosts)
+
+while 1:
+  r = nm.scan('192.168.1.1/24 -sn')
+  macs = [i['addresses'].get('mac') for i in r['scan'].values()]
+  pprint.pprint(macs)
+  time.sleep(10)
